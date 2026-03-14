00d12bf2d3f2312f3b0614a6d1deadcf91ac65ca
Amroz <amrozkhan@gmail.com>
2026-03-14 12:09:05 +0000

    Week 3 Weekend: updated constant

--- config/constants.py
+++ config/constants.py
@@ -1,11 +1,2 @@
 #config/constants.py
-
-BASE_URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-DEMO_USERNAME = "Admin"
-DEMO_PASSWORD = "admin123"
 DEFAULT_TIMEOUT = 15000 #MS
-
-Team5_URL = "https://mis-teams-team5.bromcom.dev/Nucleus/Framework/Login.aspx"
-Team5_School_ID = "851391"
-Team5_Username = "brcm"
-Team5_Password = "Br0mc0m_1234"
