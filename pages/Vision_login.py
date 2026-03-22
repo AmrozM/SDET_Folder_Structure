7ea03c21aa0dd668595cd84a11bf3babd0b1c63b
Amroz <amrozkhan@gmail.com>
2026-03-22 10:45:18 +0000

    Week5 Day 3 test fix

--- pages/Vision_login.py
+++ pages/Vision_login.py
@@ -24,5 +24,4 @@ class LoginPage:
         self.password_input.fill(password)
         self.log.info(f"Logging in with Customer code: {customer_code}")
         self.sign_in_button.click()
-
-    
+        self.log.info("MAT Vision Login successful")
